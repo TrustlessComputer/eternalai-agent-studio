e4d5a3c47ff823abb0070db9224460058cd23ea5
Peter <peter@bvm.network>
2024-12-24 11:12:30 +0700

    chore: update

--- src/modules/Studio/Studio.stories.tsx
+++ src/modules/Studio/Studio.stories.tsx
@@ -1,10 +1,24 @@
-import { Studio } from './Studio';
+import type { Meta, StoryObj } from '@storybook/react';
 
-export default {
+import { Studio } from './Studio';
+import { CREATE_MODEL_CATEGORIES } from './constants/categories';
+import { GRAPH_DATA } from './constants/data';
+
+type Story = StoryObj<typeof Studio>;
+
+const meta: Meta<typeof Studio> = {
   title: 'Studio',
   component: Studio,
 };
 
-export const Default = {
-  args: {},
+export const Default: Story = {
+  render: function useTabs() {
+    return (
+      <div style={{ border: '1px solid #000', width: '100vw', height: 'calc(100vh - 3rem)' }}>
+        <Studio categories={CREATE_MODEL_CATEGORIES} data={GRAPH_DATA} />
+      </div>
+    );
+  },
 };
+
+export default meta;

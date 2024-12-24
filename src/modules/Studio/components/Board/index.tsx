c6e31dfbf0aa60a8fdc11b0153275570c0871953
Jackie <jackie@bvm.network>
2024-12-24 16:10:12 +0700

    Refactor Board Component: Integrate Droppable and State Management
    - Added Droppable component to the Board for enhanced drag-and-drop functionality.
    - Integrated useStudioFlowStore to manage nodes and edges state.
    - Updated ReactFlow component to utilize nodes and edges from the store, along with onNodesChange and onEdgesChange handlers.

--- src/modules/Studio/components/Board/index.tsx
+++ src/modules/Studio/components/Board/index.tsx
@@ -1,15 +1,30 @@
 import { Background, Controls, ReactFlow } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
+import useStudioFlowStore from '../../stores/useStudioFlowStore';
+import Droppable from '../DnD/Droppable';
 import './Board.scss';
 
 function Board() {
+  const nodes = useStudioFlowStore((state) => state.nodes);
+  const edges = useStudioFlowStore((state) => state.edges);
+  const onNodesChange = useStudioFlowStore((state) => state.onNodesChange);
+  const onEdgesChange = useStudioFlowStore((state) => state.onEdgesChange);
+
   return (
-    <div className="board">
-      <ReactFlow fitView nodeTypes={{}} style={{ backgroundColor: '#F7F9FB' }}>
+    <Droppable id="output" data={{}} className="board">
+      <ReactFlow
+        fitView
+        nodeTypes={{}}
+        deleteKeyCode=""
+        nodes={nodes}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onNodesChange={onNodesChange}
+      >
         <Controls />
         <Background />
       </ReactFlow>
-    </div>
+    </Droppable>
   );
 }
 

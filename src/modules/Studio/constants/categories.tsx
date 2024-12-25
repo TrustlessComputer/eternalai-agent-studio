8ee90d1b596d7755a051a732b0308b2a81e1d627
Jackie <jackie@bvm.network>
2024-12-25 10:46:39 +0700

    fix data

--- src/modules/Studio/constants/categories.tsx
+++ src/modules/Studio/constants/categories.tsx
@@ -2,13 +2,15 @@ import { LegoComponentIcon } from '../components/icons/lego';
 import { StudioCategory } from '../types/category';
 
 const AGENT: StudioCategory = {
+  key: 'agent',
   keyMapper: 'agent',
   title: 'Agent',
   required: true,
   color: '#12DAC2',
   options: [
     {
-      keyMapper: 'agent-option',
+      key: 'agent-option-1',
+      keyMapper: 'agent-option-1',
       title: 'New Agent',
       tooltip: '',
       icon: 'https://storage.googleapis.com/eternal-ai/agent-studio-v2/ic_personality_nft.svg',
@@ -28,6 +30,7 @@ const AGENT: StudioCategory = {
 };
 
 const PERSONALITY: StudioCategory = {
+  key: 'personality',
   keyMapper: 'personality',
   title: 'Personality',
   required: true,
@@ -35,6 +38,7 @@ const PERSONALITY: StudioCategory = {
     'Create an agent for your NFT, Ordinals, token, —or start fresh with a new idea. This section defines your agent’s lore and backstory.',
   options: [
     {
+      key: 'personality-option-1',
       keyMapper: 'personality-option-1',
       title: 'Import from NFT',
       tooltip: '',
@@ -42,18 +46,21 @@ const PERSONALITY: StudioCategory = {
       order: 0,
     },
     {
+      key: 'personality-option-2',
       keyMapper: 'personality-option-2',
       title: 'Import from Ordinals',
       icon: 'https://storage.googleapis.com/eternal-ai/agent-studio-v2/ic_personality_ordinals.svg',
       order: 1,
     },
     {
+      key: 'personality-option-3',
       keyMapper: 'personality-option-3',
       title: 'Import from Token',
       icon: 'https://storage.googleapis.com/eternal-ai/agent-studio-v2/ic_personality_token.svg',
       order: 2,
     },
     {
+      key: 'personality-option-4',
       keyMapper: 'personality-option-4',
       title: 'New personality',
       icon: 'https://storage.googleapis.com/eternal-ai/agent-studio-v2/ic_personality_custom.svg',
@@ -65,6 +72,7 @@ const PERSONALITY: StudioCategory = {
 };
 
 const NETWORK: StudioCategory = {
+  key: 'network',
   keyMapper: 'network',
   title: 'Network',
   required: true,
@@ -72,6 +80,7 @@ const NETWORK: StudioCategory = {
     'Choose the blockchain where your agent will live. Each option comes with different deployment fees, performance levels, and ongoing costs. Pick the one that best suits your goals and budget.',
   options: [
     {
+      key: 'network-option-1',
       keyMapper: 'network-option-1',
       title: 'Import from NFT',
       tooltip: '',
@@ -79,18 +88,21 @@ const NETWORK: StudioCategory = {
       order: 0,
     },
     {
+      key: 'network-option-2',
       keyMapper: 'network-option-2',
       title: 'Import from Ordinals',
       icon: 'https://storage.googleapis.com/eternal-ai/agent-studio-v2/ic_personality_ordinals.svg',
       order: 1,
     },
     {
+      key: 'network-option-3',
       keyMapper: 'network-option-3',
       title: 'Import from Token',
       icon: 'https://storage.googleapis.com/eternal-ai/agent-studio-v2/ic_personality_token.svg',
       order: 2,
     },
     {
+      key: 'network-option-4',
       keyMapper: 'network-option-4',
       title: 'New personality',
       icon: 'https://storage.googleapis.com/eternal-ai/agent-studio-v2/ic_personality_custom.svg',

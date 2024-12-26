701375b9a37873fd8a8aea466eda07d89f997238
Peter <peter@bvm.network>
2024-12-26 18:45:35 +0700

    chore: update

--- src/modules/Studio/v1/Board/index.tsx
+++ src/modules/Studio/v1/Board/index.tsx
@@ -1,30 +1,22 @@
-import { Background, ConnectionMode, Controls, NodeTypes, OnNodeDrag, ReactFlow, useReactFlow } from '@xyflow/react';
+import { Background, ConnectionMode, Controls, NodeTypes, ReactFlow } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Droppable from '@/modules/Studio/components/DnD/Droppable';
 import useStudioFlowStore from '@/modules/Studio/stores/useStudioFlowStore';
 import useStudioFlowViewStore from '@/modules/Studio/stores/useStudioFlowViewStore';
 import { OUTPUT_DROP_ID } from '../../constants/droppable-id';
-import { NodeType } from '../../enums/node-type';
-import { StudioNode } from '../../types/graph';
 import { FlowView } from '../../types/ui';
 import './Board.scss';
 import BoardOverlay from './BoardOverlay';
-// import useDragMaskStore from '../../stores/useDragMaskStore';
 
 function Board({ nodeTypes }: { nodeTypes?: NodeTypes }) {
-  const { getIntersectingNodes } = useReactFlow();
-
   const nodes = useStudioFlowStore((state) => state.nodes);
   const edges = useStudioFlowStore((state) => state.edges);
-  const setNodes = useStudioFlowStore((state) => state.setNodes);
-  const setEdges = useStudioFlowStore((state) => state.setEdges);
   const onNodesChange = useStudioFlowStore((state) => state.onNodesChange);
   const onEdgesChange = useStudioFlowStore((state) => state.onEdgesChange);
 
   const reloadFlowCounter = useStudioFlowStore((state) => state.reloadFlowCounter);
-  const reloadFlow = useStudioFlowStore((state) => state.reloadFlow);
   const setView = useStudioFlowViewStore((state) => state.setView);
 
   const [currentView, setCurrentView] = useState<FlowView>({
@@ -54,9 +46,6 @@ function Board({ nodeTypes }: { nodeTypes?: NodeTypes }) {
         connectionMode={ConnectionMode.Loose}
         zoomOnDoubleClick={false}
         selectNodesOnDrag={false}
-        // nodesDraggable={false}
-        // onNodeDrag={onNodeDrag}
-        // onNodeDragStop={onNodeDragStop}
       >
         <Controls />
         <Background />
